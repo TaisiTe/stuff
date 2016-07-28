1f50b9104079b4976a8b3b31951fb3ec72564010
TaisiTe <taisitelve@gmail.com>
2016-07-28 22:14:32 +0300

    Update write_image_names.py

--- write_image_names.py
+++ write_image_names.py
@@ -1,10 +1,10 @@
 import os
 
-f = open('\\data\\img_name.txt', 'w')
+f = open('~\\face\\img_name.txt', 'w')
 
 d=4
 for person in range(25):
     for i in range(5):
-        f.write('data\\' + str(d) + '\\s' +str(person+1)+'\\'+str(i+1)+'.pgm\n')
+        f.write('~\\face\\data\\' + str(d) + '\\s' +str(person+1)+'\\'+str(i+1)+'.pgm\n')
 
 f.close()
