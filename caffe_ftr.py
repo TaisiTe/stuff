9893e6325e6a391bc23d7f23f9a9f620599c5ff6
TaisiTe <taisitelve@gmail.com>
2016-08-02 10:03:27 +0300

    Update caffe_ftr.py

--- caffe_ftr.py
+++ caffe_ftr.py
@@ -20,7 +20,7 @@ import scipy.io as sio
 import skimage.io
 import os
 
-caffe_root = '//home//icv//Downloads//caffe-master//'
+caffe_root = '\\opt\\caffe'
 
 import sys
 sys.path.insert(0, caffe_root + 'python')
